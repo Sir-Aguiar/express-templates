d44fed2d3925588e7c937e445d6c1410c138df1d
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-06-18 18:44:30 -0400

    feat: Base zod error handling

--- src/utils/http-handler.ts
+++ src/utils/http-handler.ts
@@ -2,7 +2,7 @@ import { Response } from "express";
 import { ProcessError } from "../entity/errors/ProcessError";
 import { ZodError } from "zod";
 
-type KnownServerErrors = ProcessError | ZodError| Error;
+type KnownServerErrors = ProcessError | ZodError | Error;
 
 export class HTTPHandler {
   constructor(private response: Response) {}
@@ -35,7 +35,31 @@ export class HTTPHandler {
     }
 
     if (error instanceof ZodError) {
-      return this.response.status(400).json({ error: { message: error.message } });
+      const { errors } = error;
+      let message = errors[0].message || "Houve um erro desconhecido na hora de validar os dados inseridos";
+
+      switch (errors[0].code) {
+        case "invalid_type":
+          message = `O campo de '${errors[0].path[0]}' está em formato inválido`;
+          break;
+        case "invalid_date" || "invalid_string":
+          message = `O campo de '${errors[0].path[0]}' está em formato inválido`;
+          break;
+        case "too_big":
+          if (errors[0].type === "string") {
+            message = `O campo de '${errors[0].path[0]}' precisa ter no máximo ${errors[0].maximum} caracteres`;
+          }
+          break;
+        case "too_small":
+          if (errors[0].type === "string") {
+            message = `O campo de '${errors[0].path[0]}' precisa ter no mínimo ${errors[0].minimum} caracteres`;
+          }
+          break;
+        default:
+          break;
+      }
+
+      return this.response.status(400).json({ error: { message } });
     }
 
     return this.response.status(500).json({});
